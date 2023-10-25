327f000032ad2b88c3e735c1bc48d1bde3ef5694
Chloe Dwyer <chloedwyer@MacBook-Pro.local>
2023-10-25 17:32:05 +0100

    Fixed databases

--- database/migrations/2023_10_24_211604_create_genre_table.php
+++ database/migrations/2023_10_24_211604_create_genre_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('genre', function (Blueprint $table) {
+        Schema::create('genres', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('description');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('genre');
+        Schema::dropIfExists('genres');
     }
 };
